1c506b2bcb01543bbfd53398edcc00c947189db1
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-07-27 19:53:43 -0700

    added order model

--- FlooringMastery/src/main/java/com/aspire/flooringmastery/model/Order.java
+++ FlooringMastery/src/main/java/com/aspire/flooringmastery/model/Order.java
@@ -5,10 +5,90 @@
  */
 package com.aspire.flooringmastery.model;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author louie
  */
 public class Order {
+
+    private Integer orderNumber;
+    private String customerName;
+    private String state;
+    private BigDecimal taxRate;
+    private String productType;
+    private BigDecimal area;
+    private BigDecimal costPerSquareFoot;
+    private BigDecimal laborCostPerSquareFoot;
+    private BigDecimal materialCost;
+    private BigDecimal laborCost;
+    private BigDecimal tax;
+    private BigDecimal total;
+
+    public Order(Integer orderNumber, String customerName, String state, BigDecimal taxRate, String productType, BigDecimal area,
+            BigDecimal costPerSquareFoot, BigDecimal laborCostPerSquareFoot, BigDecimal materialCost, BigDecimal laborCost, BigDecimal tax,
+            BigDecimal total) {
+        this.orderNumber = orderNumber;
+        this.customerName = customerName;
+        this.state = state;
+        this.taxRate = taxRate;
+        this.productType = productType;
+        this.area = area;
+        this.costPerSquareFoot = costPerSquareFoot;
+        this.laborCostPerSquareFoot = laborCostPerSquareFoot;
+        this.materialCost = materialCost;
+        this.laborCost = laborCost;
+        this.tax = tax;
+        this.total = total;
+    }
+
+    public Integer getOrderNumber() {
+        return orderNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public BigDecimal getArea() {
+        return area;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return costPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return laborCostPerSquareFoot;
+    }
+
+    public BigDecimal getMaterialCost() {
+        return materialCost;
+    }
+
+    public BigDecimal getLaborCost() {
+        return laborCost;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
 
 }
